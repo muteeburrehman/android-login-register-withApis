111e930ba46fabcb8dec3e72f3edffe6e9224152
muteeburrehman <muteeb285@gmail.com>
2024-02-22 19:17:55 +0500

    register/login with apis integration

--- app/src/main/java/pk/codebase/loginregisterwithrestapis/Login.java
+++ app/src/main/java/pk/codebase/loginregisterwithrestapis/Login.java
@@ -5,11 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import pk.codebase.requests.HttpError;
+import pk.codebase.requests.HttpRequest;
+import pk.codebase.requests.HttpResponse;
 
 public class Login extends AppCompatActivity {
 
@@ -23,24 +32,19 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        setupUI();
-        setupListeners();
-
-
-    }
-
-    private void setupUI() {
         email = findViewById(R.id.emailLog);
         password = findViewById(R.id.passwordLog);
         directToRegister = findViewById(R.id.directToRegisterPage);
         login = findViewById(R.id.login);
-    }
 
-    private void setupListeners() {
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                checkUsername();
+
+                checkDataEntered();
+                Intent intent = new Intent(Login.this,MainActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -52,19 +56,83 @@ public class Login extends AppCompatActivity {
             }
         });
     }
- void checkUsername(){
-        boolean isValid = true;
-        if(isEmpty(email) && !isEmail(email)){
-            email.setError("Write correct email");
-            isValid = false;
+
+
+    void checkDataEntered(){
+
+
+        String userEmail = email.getText().toString();
+        String userPassword = password.getText().toString();
+
+
+
+        if (isEmpty(email) && isEmpty(password)) {
+            email.setError("Field required");
+            password.setError("Field required");
+        } else if (isEmpty(password)) {
+            password.setError("Field required");
+        } else {
+            loginUser(userEmail, userPassword);
         }
 
-        if (isEmpty(password)) {
-            password.setError("You must enter password to login!");
-            isValid = false;
+        if (isEmailValid(email) == false){
+            email.setError("Enter valid email!");
         }
- }
-    boolean isEmail(EditText text) {
+    }
+// void checkUsername(){
+//        boolean isValid = true;
+//        if(isEmpty(email) && !isEmail(email)){
+//            email.setError("Write correct email");
+//            isValid = false;
+//        }
+//
+//        if (isEmpty(password)) {
+//            password.setError("You must enter password to login!");
+//            isValid = false;
+//        }
+// }
+     private void loginUser(String email, String password){
+         HttpRequest request = new HttpRequest();
+         request.setOnResponseListener(new HttpRequest.OnResponseListener() {
+             @Override
+             public void onResponse(HttpResponse response) {
+                 if (response.code == HttpResponse.HTTP_OK) {
+                     System.out.println(response.toJSONObject());
+                     JSONObject obj = response.toJSONObject();
+                     Toast.makeText(Login.this, "User logged in", Toast.LENGTH_SHORT).show();
+                     try {
+                         String token = obj.getString("access_token");
+                         Log.e("token", token);
+                     } catch (JSONException e) {
+                         throw new RuntimeException(e);
+                     }
+                 }else {
+                     Toast.makeText(Login.this, "User not logged in! " + response.code, Toast.LENGTH_SHORT).show();
+                     Log.e("error", response.toString() + ",.,." + response.code + ",.,." + response.reason);
+
+                 }
+             }
+         });
+         request.setOnErrorListener(new HttpRequest.OnErrorListener() {
+             @Override
+             public void onError(HttpError error) {
+                 // There was an error, deal with it
+                 Log.e("error", error.toString() + ",.,." + error.code + ",.,." + error.reason);
+                 Toast.makeText(Login.this, "User not logged in!" + error.code +",.,."+ error.toString(), Toast.LENGTH_SHORT).show();
+             }
+         });
+
+         JSONObject json;
+         try {
+             json = new JSONObject();
+             json.put("email", email);
+             json.put("password", password);
+         } catch (JSONException ignore) {
+             return;
+         }
+         request.post("http://192.168.0.180:8080/login", json);
+     }
+    boolean isEmailValid(EditText text) {
         CharSequence email = text.getText().toString();
         return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
     }
--- app/src/main/java/pk/codebase/loginregisterwithrestapis/Register.java
+++ app/src/main/java/pk/codebase/loginregisterwithrestapis/Register.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -12,10 +13,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import pk.codebase.requests.HttpError;
+import pk.codebase.requests.HttpRequest;
+import pk.codebase.requests.HttpResponse;
+
 public class Register extends AppCompatActivity {
-    EditText username;
-    EditText email;
-    EditText password;
+    EditText username, email, password;
     Button register;
     TextView directToLogin;
     @Override
@@ -28,7 +34,8 @@ public class Register extends AppCompatActivity {
         password = findViewById(R.id.password);
         register = findViewById(R.id.register);
         directToLogin = findViewById(R.id.directToLoginPage);
-        
+
+
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -57,13 +64,66 @@ public class Register extends AppCompatActivity {
     }
 
     void checkDataEntered(){
+
+        String name = username.getText().toString();
+        String userEmail = username.getText().toString();
+        String userPassword = username.getText().toString();
+
         if(isEmpty(username)){
+            username.setError("Field required");
             Toast t = Toast.makeText(this,"You must enter username to register!",Toast.LENGTH_SHORT);
             t.show();
+        }
+
+        if (isEmpty(username) && isEmpty(email) && isEmpty(password)) {
+            username.setError("Field required");
+            email.setError("Field required");
+            password.setError("Field required");
+        } else if (isEmpty(password)) {
+            password.setError("Field required");
+        } else {
+            registerUser(name, userEmail, userPassword);
         }
 
         if (isEmailValid(email) == false){
             email.setError("Enter valid email!");
         }
+    }
+
+    private void registerUser(String userName, String email, String password) {
+        HttpRequest request = new HttpRequest();
+        request.setOnResponseListener(new HttpRequest.OnResponseListener() {
+            @Override
+            public void onResponse(HttpResponse response) {
+                if (response.code == HttpResponse.HTTP_OK) {
+                    System.out.println(response.toJSONObject());
+                    Toast.makeText(Register.this, "User registered!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(Register.this, "User not registered! " + response.code, Toast.LENGTH_SHORT).show();
+                    Log.e("error", response.toString() + ",.,." + response.code + ",.,." + response.reason);
+
+                }
+            }
+        });
+        request.setOnErrorListener(new HttpRequest.OnErrorListener() {
+            @Override
+            public void onError(HttpError error) {
+                // There was an error, deal with it
+                Log.e("error", error.toString() + ",.,." + error.code + ",.,." + error.reason);
+                Toast.makeText(Register.this, "User not  registered!" + error.code +",.,."+ error.toString(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        JSONObject json;
+        try {
+            json = new JSONObject();
+            json.put("username", userName);
+            json.put("email", email);
+            json.put("password", password);
+        } catch (JSONException ignore) {
+            return;
+        }
+        request.post("http://192.168.0.180:8080/register", json);
     }
 }
